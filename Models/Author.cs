4485d6d5819bfceda042bb504fdf6f64352b52a0
ALEX-CRISTIAN SUPURAN <alex.supuran@stud.ubbcluj.ro>
2023-11-05 23:47:47 +0200

    Laboratorul 4, doar ca fara sarcina de laborator, deoarece am uitat de ea si timpul nu a mai permis sa o fac inainte de termenul limita

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,14 @@
-﻿namespace Supuran_Alex_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Supuran_Alex_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+     
+  
         public ICollection<Book>? Books { get; set; }
     }
 }
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,14 @@
+﻿using Supuran_Alex_Lab2.Models;
+
+namespace Supuran_Alex_Lab2.ViewModels
+{
+ 
+        public class PublisherIndexData
+        {
+
+            public IEnumerable<Publisher> Publishers { get; set; }
+            public IEnumerable<Book> Books { get; set; }
+
+        }
+
+}
